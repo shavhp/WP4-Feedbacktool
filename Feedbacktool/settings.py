eaccb434d47cdb280edce23f94c566106a5da57b
Kangyou123 <0987432@hr.nl>
2023-04-13 10:20:56 +0200

    Modify settings.py

--- Feedbacktool/settings.py
+++ Feedbacktool/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "login.apps.LoginConfig",
+    "Login.apps.LoginConfig",
     "ccforms.apps.CcformsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
