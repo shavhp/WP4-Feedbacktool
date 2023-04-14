aee93ddccb306f236e25cbecd8052d979eef0713
shavhp <s.plein92@outlook.com>
2023-04-14 19:28:23 +0200

    Creates models and superusers in django admin

--- ccforms/admin.py
+++ ccforms/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class McQuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "mc_question_id",
+        "question",
+        "option_a",
+        "option_b",
+        "option_c",
+        "option_d",
+    )
+
+
+class OpenQuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "open_question_id",
+        "question",
+    )
+
+
+class AdministratorAdmin(admin.ModelAdmin):
+    list_display = (
+        "admin_id",
+        "email",
+        "last_name",
+        "first_name",
+        "is_admin",
+    )
+
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = (
+        "team_member_id",
+        "email",
+        "last_name",
+        "first_name",
+        "is_admin",
+    )
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = (
+        "survey_id",
+        "admin",
+        "title",
+        "description",
+        "is_anonymous",
+        "date_sent",
+        "open_question",
+        "mc_question",
+        "url",
+    )
+
+
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = (
+        "response_id",
+        "survey",
+        "tm_email",
+        "open_question",
+        "mc_question",
+        "response",
+        "date_submitted",
+    )
+
+
+admin.site.register(McQuestion, McQuestionAdmin)
+admin.site.register(OpenQuestion, OpenQuestionAdmin)
+admin.site.register(Administrator, AdministratorAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Response, ResponseAdmin)
--- ccforms/models.py
+++ ccforms/models.py
@@ -1,4 +1,72 @@
 from django.db import models
 
+
 # Create your models here.
-# type ur models here
+class McQuestion(models.Model):
+    mc_question_id = models.AutoField(primary_key=True, verbose_name="Vraagnummer")
+    question = models.CharField(verbose_name="Vragen", max_length=250)
+    option_a = models.CharField(verbose_name="Optie A", max_length=250)
+    option_b = models.CharField(verbose_name="Optie B", max_length=250)
+    option_c = models.CharField(verbose_name="Optie C", max_length=250, null=True)
+    option_d = models.CharField(verbose_name="Optie D", max_length=250, null=True)
+
+    class Meta:
+        verbose_name = "Multiple Choice Question"
+
+    def __str__(self):
+        return self.question[:50]
+
+
+class OpenQuestion(models.Model):
+    open_question_id = models.AutoField(primary_key=True, verbose_name="Vraagnummer")
+    question = models.CharField(verbose_name="Vragen", max_length=250)
+
+    def __str__(self):
+        return self.question[:50]
+
+
+class Administrator(models.Model):
+    admin_id = models.AutoField(primary_key=True, verbose_name="Administratornummer")
+    email = models.EmailField(max_length=250, verbose_name="E-mailadres")
+    last_name = models.CharField(max_length=250, verbose_name="Achternaam")
+    first_name = models.CharField(max_length=250, verbose_name="Voornaam")
+    is_admin = models.BooleanField(default=True, verbose_name="Is administrator")
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+
+class TeamMember(models.Model):
+    team_member_id = models.AutoField(primary_key=True, verbose_name="Teamlidnummer")
+    email = models.EmailField(max_length=250, verbose_name="E-mailadres")
+    last_name = models.CharField(max_length=250, verbose_name="Achternaam")
+    first_name = models.CharField(max_length=250, verbose_name="Voornaam")
+    is_admin = models.BooleanField(default=False, verbose_name="Is administrator")
+
+    def __str__(self):
+        return self.email
+
+
+class Survey(models.Model):
+    survey_id = models.AutoField(primary_key=True, verbose_name="Enquêtenummer")
+    admin = models.ForeignKey(Administrator, on_delete=models.CASCADE, default="", verbose_name="Administrator")
+    title = models.CharField(max_length=100, verbose_name="Naam enquête")
+    description = models.CharField(max_length=500, verbose_name="Toelichting")
+    is_anonymous = models.BooleanField(default=True, verbose_name="Anonieme respons")
+    date_sent = models.DateField(null=True, verbose_name="Verzonden op")
+    open_question = models.ForeignKey(OpenQuestion, on_delete=models.CASCADE, default="", verbose_name="Open vragen")
+    mc_question = models.ForeignKey(McQuestion, on_delete=models.CASCADE, default="", verbose_name="Meerkeuzevragen")
+    url = models.URLField(max_length=200, default="", unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Response(models.Model):
+    response_id = models.AutoField(primary_key=True, verbose_name="Responsnummer")
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, default="", verbose_name="Naam enquête")
+    tm_email = models.ForeignKey(TeamMember, on_delete=models.CASCADE, default="", verbose_name="E-mailadres teamlid")
+    open_question = models.ForeignKey(OpenQuestion, on_delete=models.CASCADE, default="", verbose_name="Open vragen")
+    mc_question = models.ForeignKey(McQuestion, on_delete=models.CASCADE, default="", verbose_name="Meerkeuzevragen")
+    response = models.CharField(max_length=500, verbose_name="Reacties")
+    date_submitted = models.DateField(null=True, verbose_name="Ingevuld op")
