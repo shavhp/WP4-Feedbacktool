d8687ef37f6356aac45a9e96135c2419e84dfcd8
Sharelle-0825486 <0825486@hr.nl>
2023-05-17 10:45:40 +0200

    FEATURE: Implements sidebar with working route

--- Frontend/rccforms/src/App.js
+++ Frontend/rccforms/src/App.js
@@ -1,16 +1,86 @@
+// Sidebar sources:
+// https://blog.openreplay.com/simple-sidebars-with-react-pro-sidebar-and-material-ui/
+// https://blog.logrocket.com/creating-responsive-sidebar-react-mui/
+
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
 import UserList from "./GetUser";
-import OpenQuestions from "./pages/Questions";
+import Questions from "./pages/Questions";
+
+// Imports MUI icons from:
+// https://mui.com/material-ui/material-icons/?theme=Outlined
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined';
+import QuizOutlinedIcon from '@mui/icons-material/QuizOutlined';
+import Diversity3OutlinedIcon from '@mui/icons-material/Diversity3Outlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
 
 function App() {
+    const { collapseSidebar } = useProSidebar();
+
     return (
-        <Router>
-            <Routes>
-                <Route path='/userlist' element={<UserList/>}/>
-                <Route path='/questions' element={<OpenQuestions/>}/>
-            </Routes>
-        </Router>
+        <div
+            id="app"
+            style={(
+                { height: "100vh" },
+                    { display: "flex" }
+            )}
+        >
+            <Sidebar
+                style={{ height: "100vh" }}
+                onClick={() => {
+                    collapseSidebar();
+                }}
+            >
+                <Menu>
+                    <MenuItem
+                        icon={<MenuOutlinedIcon/>}
+                        style={{ textAlign: "center" }}
+                    >
+                        {" "}
+                        {/* Get username / fname function needed here */}
+                        <h2>Username</h2>
+                    </MenuItem>
+                    <MenuItem
+                        icon={<HomeOutlinedIcon/>}
+                    >
+                        Home
+                    </MenuItem>
+                    <MenuItem
+                        icon={<Diversity3OutlinedIcon/>}
+                    >
+                        Teamleden
+                    </MenuItem>
+
+                    <MenuItem
+                        icon={<QuizOutlinedIcon/>}
+                        component={<Link
+                            to="/questions"/>}
+                    >
+                        Vragen
+                    </MenuItem>
+
+                    <MenuItem
+                        icon={<NoteAltOutlinedIcon/>}
+                    >
+                        Vragenlijsten
+                    </MenuItem>
+                    <MenuItem
+                        icon={<AccountCircleOutlinedIcon/>}
+                    >
+                        Mijn account
+                    </MenuItem>
+                </Menu>
+            </Sidebar>
+
+                <Routes>
+                    <Route path='/userlist' element={<UserList/>}/>
+                    <Route path='/questions' element={<Questions/>}/>
+                </Routes>
+        </div>
     );
 }
 
--- Frontend/rccforms/src/index.js
+++ Frontend/rccforms/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-            <App/>
+        <ProSidebarProvider>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </ProSidebarProvider>
     </React.StrictMode>
 );
