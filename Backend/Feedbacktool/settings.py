e4f4272628c5aff670c9b98a22b6177dcad15915
Mustafa566 <bolat31_@hotmail.com>
2023-05-17 15:49:48 +0200

    Adding new startApp because auth is in use

--- Backend/Feedbacktool/settings.py
+++ Backend/Feedbacktool/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "ccforms.apps.CcformsConfig",
     'rest_framework',
     'corsheaders',
+    #'auth',
 ]
 
 MIDDLEWARE = [
@@ -127,16 +128,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-
 CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     # Add any other allowed origins here
 ]
-
 CSRF_COOKIE_DOMAIN = 'localhost:3000'
 CORS_ALLOW_CREDENTIALS = True
+
+# CustomUser setting
+#AUTH_USER_MODEL = 'auth.CustomUser'
