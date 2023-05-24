98be2a87ef4faac08154d6ee93a1197827eb97be
Kangyou123 <0987432@hr.nl>
2023-05-24 11:21:38 +0200

    css added to forms

--- Frontend/rccforms/src/components/making_forms/Forms.js
+++ Frontend/rccforms/src/components/making_forms/Forms.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { API_URL_SURVEYS, API_URL_QUESTIONS, API_URL_MC_OPTIONS } from "../../constants";
+import "./Forms.css";
 
 function Forms() {
   const [surveys, setSurveys] = useState([]);
+  const [expandedSurvey, setExpandedSurvey] = useState(null);
 
   useEffect(() => {
     fetch(API_URL_SURVEYS)
@@ -28,32 +30,44 @@ function Forms() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleSurveyClick = (surveyId) => {
+    if (expandedSurvey === surveyId) {
+      setExpandedSurvey(null);
+    } else {
+      setExpandedSurvey(surveyId);
+    }
+  };
+
   return (
-    <div>
-      <h1>Forms</h1>
-      <h2>Alle Forms:</h2>
+    <div className="forms-container">
+      <h1 className="forms-title">Forms</h1>
+      <h2 className="forms-subtitle">Alle Forms:</h2>
       {surveys.map((survey) => (
-        <div key={survey.survey_id}>
-          <h3>{survey.title}</h3>
-          <p>{survey.description}</p>
-          <p>Survey ID: {survey.survey_id}</p>
-          <p>Admin: {survey.admin}</p>
-          <p>Is Anonymous: {survey.is_anonymous ? "Yes" : "No"}</p>
-          <p>Date Sent: {survey.date_sent}</p>
-          <p>Open Vragen:</p>
-          <ul>
-            {survey.questions.map((question) => (
-              <li key={question.id}>{question.question_text}</li>
-            ))}
-          </ul>
-          <p>Meerkeuzevragen:</p>
-          <ul>
-            {survey.multiple_choice.map((mcQuestion) => (
-              <li key={mcQuestion.id}>{mcQuestion.question_text}</li>
-            ))}
-          </ul>
+        <div className="survey-card" key={survey.survey_id}>
+          <h3 onClick={() => handleSurveyClick(survey.survey_id)}>{survey.title}</h3>
+          {expandedSurvey === survey.survey_id && (
+            <>
+              <p>Survey ID: {survey.survey_id}</p>
+              <p>Admin: {survey.admin}</p>
+              <p>Is Anonymous: {survey.is_anonymous ? "Yes" : "No"}</p>
+              <p>Date Sent: {survey.date_sent}</p>
+              <p>Open Vragen:</p>
+              <ul>
+                {survey.questions.map((question) => (
+                  <li key={question.id}>{question.question_text}</li>
+                ))}
+              </ul>
+              <p>Meerkeuzevragen:</p>
+              <ul>
+                {survey.multiple_choice.map((mcQuestion) => (
+                  <li key={mcQuestion.id}>{mcQuestion.question_text}</li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
       ))}
+      <button className="add-button">Toevoegen</button>
     </div>
   );
 }
