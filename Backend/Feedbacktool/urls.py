0b14b287f1cca058b003911de219cd25fffbf550
Maarten <m.deleeuwvanweenen2003@gmail.com>
2023-06-07 00:03:15 +0200

    ADDED: correct(?) url for editing

--- Backend/Feedbacktool/urls.py
+++ Backend/Feedbacktool/urls.py
@@ -22,10 +22,10 @@ from ccforms import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', UserList.as_view()),
-    path('api/open_questions/', views.open_q_list),
-    path('api/open_questions/<int:pk>/edit', views.edit_open_q),
-    path('api/mc_questions/', views.mc_q_list),
     path('api/currentUser/', views.current_user),
+    path('api/open_questions/', views.open_q_list),
     path('api/open_questions/<int:pk>/hide/', views.hide_open_q),
+    path('api/open_questions/<int:pk>/edit/', views.edit_open_q),
+    path('api/mc_questions/', views.mc_q_list),
     path('api/mc_questions/<int:pk>/hide/', views.hide_mc_q),
 ]
